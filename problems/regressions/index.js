5875dc9c3d6d85a01c16000e96fdf543407c390b
Tiffany Poss <tiffanyposs@.com>
2018-12-13 17:28:46 -0500

    add comments to regression

--- problems/regressions/index.js
+++ problems/regressions/index.js
@@ -4,6 +4,7 @@ const loadCSV = require('./load-csv');
 const LinearRegression = require('./linear-regression');
 const plot = require('node-remote-plot');
 
+// extract the data from the loadCSV module
 let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
 	shuffle: true,
 	splitTest: 50,
@@ -11,21 +12,24 @@ let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
 	labelColumns: ['mpg'],
 });
 
+// create a new regression, pass in feature, labels, and options
 const regression = new LinearRegression(features, labels, {
 	learningRate: 0.1,
 	iterations: 100,
 });
 
+// train the regression
 regression.train();
+
+// test the regression's success
+// you're looking for r2 to be a low positive number.
 const r2 = regression.test(testFeatures, testLabels);
 
+// create a graph
 plot({
 	x: regression.mseHistory.reverse(),
 	xLabel: 'Iteration #',
 	yLabel: 'Mean Squared Error',
 });
 
-// your looking for r2 to be a low positive number. If it is negative, that means
-// just taking the average is better than the answer you're getting. So you want a positive
-// number
 console.log('R2 is ', r2);
--- problems/regressions/linear-regression.js
+++ problems/regressions/linear-regression.js
@@ -1,26 +1,38 @@
 const tf = require('@tensorflow/tfjs');
 const _ = require('lodash');
 
-
 class LinearRegression {
 
-	// setup defaults
+	/**
+	 * Sets up all default properties
+	 * @name constructor
+	 * @param features - matrix of features
+	 * @param labels - matrix of features
+	 * @param options - override default options
+	*/
 	constructor(features, labels, options) {
-		this.features = this.processFeatures(features);
-		this.labels = tf.tensor(labels);
-		this.mseHistory = [];
+		this.features = this.processFeatures(features); // all features
+		this.labels = tf.tensor(labels); // all labels
+		this.mseHistory = []; // history of Mean Square Error
 
 		this.options = Object.assign({
 			learningRate: 0.1,
 			iterations: 1000
 		}, options);
 
-		this.weights = tf.zeros([this.features.shape[1], 1]);
+		this.weights = tf.zeros([this.features.shape[1], 1]); // m and b values
 	}
 
+	/**
+	 * multiply the features with the weights (m & b guesses)
+	 * get the differences between the current guesses and the labels
+	 * find the average slope between the features and the differences
+	 * update the weights (m & b) to be the weights minus the slopes * learning rate
+	 * @name gradientDescent
+	*/
 	gradientDescent() {
 		// matrix multiplication
-		const currentGuesses = this.features.matMul(this.weights)
+		const currentGuesses = this.features.matMul(this.weights);
 		const differences = currentGuesses.sub(this.labels);
 		const slopes = this.features
 		  .transpose()
@@ -30,7 +42,13 @@ class LinearRegression {
 		this.weights = this.weights.sub(slopes.mul(this.options.learningRate))
 	}
 
-	// loop through each allowed iteration
+	/**
+	 * Initializes the training
+	 * loop through each allowed iteration and update the gradient Decent
+	 * recored the Mean Square Error
+	 * update the learning rate
+	 * @name train
+	*/
 	train() {
 		for (let i = 0; i < this.options.iterations; i++) {
 			this.gradientDescent();
@@ -39,42 +57,76 @@ class LinearRegression {
 		}
 	}
 
+	/**
+	 * Test the results
+	 * process the testFeatures and testLabels the same way as for training data
+	 * make predictions based on the final (m & b) (this.weights) by multiplying testFeatures matrix by the weights
+	 * get the res (sum of squares of residuals) by subtracting the predictions from the actual results, square all of them, add them together, get the value
+	 * get the tot (total sum of squares) by subtracting the mean of the real data from the real data, square it, sum it, get the value
+	 * return 1 minus the res / tot
+	 * @name test
+	 * @param testFeatures
+	 * @param testLabels
+	 * @return {Number}
+	*/
 	test(testFeatures, testLabels) {
 		testFeatures = this.processFeatures(testFeatures);
 		testLabels = tf.tensor(testLabels);
+
 		const predictions = testFeatures.matMul(this.weights);
-
-		// subtract the predictions from the real data, square all of them, add them together, get the value
 		const res = testLabels.sub(predictions).pow(2).sum().get();
-
-		// subtract the mean of the real data from the real data, square it, sum it, get the value
 		const tot = testLabels.sub(testLabels.mean()).pow(2).sum().get();
 
 		return 1 - res / tot;
 	}
 
+	/**
+	  * Process a feature set of data
+		* convert into a tensor
+		* standardize the features
+		* lastly, create a ones matrix with the number of rows of the feature set and concat with with features
+		* @name processFeatures
+		* @param features {Array}
+		* @return {Tensor}
+	*/
 	processFeatures(features) {
 		features = tf.tensor(features);
-
-		if (this.mean && this.variance) {
-			features = features.sub(this.mean).div(this.variance.pow(0.5))
-		} else {
-			features = this.standardize(features);
-		}
-
+		features = this.standardize(features);
 		features = tf.ones([features.shape[0], 1]).concat(features, 1);
 
 		return features;
 	}
 
+	/**
+	 * If the mean and variance already exist, run the standardization on the existing mean and variance
+	 * If not, get the mean and variance by using tf.moments on features
+	 * set the mean and variance props
+	 * then run the standardization on the updated mean and variance
+	 * @name standardize
+	 * @param features
+	*/
 	standardize(features) {
+		if(this.mean && this.variance) {
+			return features.sub(this.mean).div(this.variance.pow(0.5));
+		}
+
 		const { mean, variance } = tf.moments(features, 0);
 		this.mean = mean;
 		this.variance = variance;
+
 		return features.sub(mean).div(variance.pow(0.5));
 	}
 
-	// records mean square error
+	/**
+	  * Tracks the Mean Square Error history
+		* Matrix Multiply the features by the weights (m & b)
+		* subtract the labels
+		* square it
+		* add them all together
+		* divide by the number of features
+		* add it to the mseHistory prop
+		* @name recordMSE
+	*/
 	recordMSE() {
 		const mse = this.features
 			.matMul(this.weights)
@@ -87,6 +139,13 @@ class LinearRegression {
 		this.mseHistory.unshift(mse);
 	}
 
+	/**
+	 * Dynamically updates the learning rate
+	 * if the mean square error history doesn't have enough history, skip
+	 * if the last MSE is greater than the second to last MSE ---> divide the learning rate by half (it's way too big)
+	 * else increase the learning rate by 5% ( make it slightly more in the right direction)
+	 * @name updateLearningRate
+	*/
 	updateLearningRate() {
 		if (this.mseHistory.length < 2) return;
 		if (this.mseHistory[0] > this.mseHistory[1]) {
@@ -97,8 +156,5 @@ class LinearRegression {
 	}
 
 }
-
-
-
 
 module.exports = LinearRegression;
